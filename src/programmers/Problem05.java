6fcf9ee2a9431a49cc06793069ca662db84dd5b4
D0ri123 <lsh81550@gmail.com>
2023-03-13 16:09:37 +0900

    solve 단속 카메라

--- src/programmers/Problem05.java
+++ src/programmers/Problem05.java
@@ -1,2 +1,59 @@
-package programmers;public class Problem05 {
+package programmers;
+
+// 단속 카메라
+
+import java.util.Arrays;
+
+class Car implements Comparable<Car> {
+  int in;
+  int out;
+
+  public Car(int in, int out) {
+    this.in = in;
+    this.out = out;
+  }
+
+  @Override
+  public int compareTo(Car car) {
+    if(this.in == car.in) {
+      return this.out - car.out;
+    }
+    return this.in - car.in;
+  }
+}
+
+public class Problem05 {
+  public static Car[] arr;
+  public static void main(String[] args) {
+    Problem05 sol = new Problem05();
+    int[][] routes = {{2,2},{0,1},{-10,9}};
+
+    arr = new Car[routes.length];
+    for(int i=0; i<routes.length ; i++) {
+      Car car = new Car(routes[i][0], routes[i][1]);
+      arr[i] = car;
+    }
+
+    Arrays.sort(arr);
+    System.out.println(sol.solution());
+  }
+
+  public int solution() {
+    int answer = 1;
+    int bound = arr[0].out;
+
+    for(int i=1; i<arr.length; i++) {
+      if(bound >= arr[i].out) {
+        bound = arr[i].out;
+      }
+
+      if(bound < arr[i].in) {
+        answer ++;
+        bound = arr[i].out;
+      }
+    }
+    return answer;
+  }
+
+
 }
