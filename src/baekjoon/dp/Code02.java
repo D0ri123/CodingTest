5f97638ac42cda614fc30a792c6fa2e1dd851475
D0ri123 <lsh81550@gmail.com>
2023-03-23 13:43:35 +0900

    solve 피보나치 수2

--- src/baekjoon/dp/Code02.java
+++ src/baekjoon/dp/Code02.java
@@ -1,2 +1,23 @@
-package baekjoon.dp;public class Code02 {
+package baekjoon.dp;
+
+// 피보나치 수 2
+
+import java.util.Scanner;
+
+public class Code02 {
+  public static long[] fibo;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    fibo = new long[n+1];
+    fibo[0] = 0;
+    fibo[1] = 1;
+
+    for(int i=2; i<=n; i++) {
+      fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+
+    System.out.println(fibo[n]);
+  }
 }
