5eccdfc0a39ce2b7fd1ed7d7e1974e24896a8a7f
D0ri123 <lsh81550@gmail.com>
2023-04-03 20:21:28 +0900

    solve 가장 긴 프로그래밍

--- src/programmers/Problem11.java
+++ src/programmers/Problem11.java
@@ -1,2 +1,89 @@
-package programmers;public class Problem11 {
+package programmers;
+
+//가장 긴 팰린드롬
+
+public class Problem11 {
+  public static String[] arr;
+  public static void main(String[] args) {
+    String s = "abacde";
+
+    Problem11 sol = new Problem11();
+    System.out.println(sol.solution(s));
+  }
+
+  public int solution(String s) {
+    String[] arr = s.split("");
+    int result = Integer.MIN_VALUE;
+
+    String initReversed = new StringBuilder(s).reverse().toString();
+
+    if(s.length() < 2) {
+      return 1;
+    } else if(s.equals(initReversed)) {
+      return s.length();
+    } else {
+      int lt = 0;
+      int rt = 1;
+
+      while(lt != s.length()-2) {
+        if(rt==s.length()-1) {
+          lt++;
+          rt = lt+1;
+        }
+        if(!arr[lt].equals(arr[rt])) {
+          rt++;
+        } else {
+          String str = connect(lt, rt, arr);
+          String reversed = new StringBuilder(str).reverse().toString();
+          if(str.equals(reversed)) {
+            result = Math.max(result, str.length());
+          }
+          rt++;
+        }
+      }
+    }
+    return result;
+  }
+
+  private String connect(int lt, int rt, String[] arr) {
+    StringBuilder sb = new StringBuilder();
+
+    int index=lt;
+    while(index <= rt) {
+      sb.append(arr[index]);
+      index++;
+    }
+
+    return sb.toString();
+  }
+
+  public int twoMid(int lt, int rt) {
+    int count = 2;
+    lt = lt-1;
+    rt = rt+1;
+    return counting(count, lt, rt);
+  }
+
+  public int oneMid(int mid) {
+    int count = 1;
+    int lt = mid - 1;
+    int rt = mid + 1;
+    return counting(count, lt, rt);
+  }
+
+  private int counting(int count, int lt, int rt) {
+    while(lt >= 0 && rt < arr.length) {
+      if(arr[lt].equals(arr[rt])) {
+        count = count+2;
+        if(lt == 0 || rt==arr.length-1) {
+          break;
+        } else {
+          lt--;
+          rt++;
+        }
+      } else break;
+    }
+    return count;
+  }
+
 }
